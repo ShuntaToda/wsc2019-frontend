d064deb2e25021f062b1caa4919e8b493c127439
ShuntaToda <todashuntawork@gmail.com>
2024-06-05 13:49:47 +0900

    event detail取得

--- src/apis/events.js
+++ src/apis/events.js
@@ -22,3 +22,24 @@ export const getEventsApi = async (attendee) => {
     return false
   }
 }
+
+export const getEventDetailApi = async (event) => {
+
+  const path = appPath + `organizers/${event.organizer.slug}/events/${event.slug}`
+  try {
+    const res = await fetch(path, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      }
+    })
+
+    const data = await res.json()
+    console.log(data)
+    return data
+  } catch (e) {
+    console.error(e)
+    return false
+  }
+}
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getEventDetailApi } from '../apis/events'
 
 export const Event = ({ page, setPage, attendee }) => {
+  const [eventDetail, setEventDetail] = useState({})
+  const getEventDetail = async () => {
+    const data = await getEventDetailApi(page.data.event);
+    if (data) setEventDetail(data)
+  }
   useEffect(() => {
-    if (page.data.event === undefined) setPage({ section: "eventList" })
+    if (page.data.event === undefined) return setPage({ section: "eventList" })
+    getEventDetail()
   }, [])
   return (
     <div>
       <h2>{page.data.event.name}</h2>
+      <div>
+
+      </div>
     </div>
   )
 }
